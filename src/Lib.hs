8b819be60e1bf3bbe2d8b1679741a50893f03502
rami3l <rami3l@outlook.com>
2022-05-30 21:19:43 +0200

    chore(deps): add basic dev deps

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,20 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    someFunc,
+) where
+
+import Data.String.Interpolate
+import Optics (makeFieldLabels)
+import Optics.Operators
+import Relude
+
+data SomeStruct = SomeStruct
+    { foo :: Int
+    , bar :: String
+    }
+    deriving (Show, Generic)
+makeFieldLabels ''SomeStruct
 
 someFunc :: IO ()
-someFunc = putStrLn "Hello, World!"
+someFunc = putStrLn [i|Hello, #{struct ^. #foo} #{struct ^. #bar}!|]
+  where
+    struct = SomeStruct 42 "fishes"
