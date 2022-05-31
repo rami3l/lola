a84a74f3b8438f1dec0659a23c20c5db9bfb7055
rami3l <rami3l@outlook.com>
2022-05-31 08:29:26 +0200

    refactor: replace `parsec` with `megaparsec`

--- src/Lola/Parser.hs
+++ src/Lola/Parser.hs
@@ -1,30 +1,35 @@
-module Lola.Parser where
+module Lola.Parser () where
 
+import Optics (makeFieldLabels)
 import Optics.Operators
 import Relude
-import Text.Parsec (SourcePos, getPosition)
-import Text.Parsec.Language (emptyDef, javaStyle)
-import Text.Parsec.String (Parser)
-import Text.Parsec.Token (GenLanguageDef)
-import qualified Text.Parsec.Token as Token
+import Text.Megaparsec (MonadParsec (notFollowedBy, try), Parsec, ParsecT, SourcePos, getSourcePos)
+import Text.Megaparsec.Char (alphaNumChar, space1, string)
+import qualified Text.Megaparsec.Char.Lexer as L
 
-lexer :: Token.TokenParser ()
-lexer =
-  Token.makeTokenParser
-    javaStyle
-      { Token.reservedOpNames = [".", ",", "+", "*", "-", "/", "!", "!=", "=", "==", ">", ">=", "<", "<="] ++ kwOps,
-        Token.reservedNames = ["break", "class", "continue", "else", "false", "fun", "for", "if", "nil", "print", "return", "super", "this", "true", "var", "while"] ++ kwOps,
-        Token.caseSensitive = Relude.True
-      }
-  where
-    kwOps = ["and", "or"]
-
-data Token = Token
-  { tokenType :: TokenType,
-    lexeme :: String,
-    pos :: SourcePos
-  }
-  deriving (Show)
+keywords :: [Text] -- list of reserved words
+keywords =
+  [ -- Regular keywords.
+    "break",
+    "class",
+    "continue",
+    "else",
+    "false",
+    "fun",
+    "for",
+    "if",
+    "nil",
+    "print",
+    "return",
+    "super",
+    "this",
+    "true",
+    "var",
+    "while",
+    -- Keyword operators.
+    "and",
+    "or"
+  ]
 
 data TokenType
   = Comma
@@ -63,17 +68,51 @@ data TokenType
   | True
   | Var
   | While
-  | SingleLineComment
   deriving (Show)
 
-reservedOp :: String -> Parser Token
-reservedOp str = do
-  pos <- getPosition
-  lexer `Token.reservedOp` str
-  return $ Token Comma str pos
+data Token = Token
+  { tokenType :: TokenType,
+    tokenLexeme :: Text,
+    tokenPos :: SourcePos
+  }
+  deriving (Show)
+
+makeFieldLabels ''Token
+
+type Parser :: Type -> Type
+type Parser = Parsec Void Text
+
+space :: Parser ()
+space =
+  L.space
+    space1
+    (L.skipLineComment "//")
+    (L.skipBlockCommentNested "/*" "*/")
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme space
+
+symbol :: Text -> Parser Text
+symbol = L.symbol space
+
+-- | A reserved symbol in the Lox language.
+rsym :: TokenType -> Text -> Parser Token
+rsym tkType str = do
+  pos <- getSourcePos
+  str' <- symbol str
+  return $ Token tkType str' pos
+
+-- | A reserved keyword in the Lox language.
+rword :: TokenType -> Text -> Parser Token
+rword tkType str = do
+  pos <- getSourcePos
+  str' <- lexeme . try $ string str <* notFollowedBy alphaNumChar
+  return $ Token tkType str' pos
+
+-- TODO: Refactor the two functions above.
 
 comma, dot, minus, plus :: Parser Token
-comma = reservedOp ","
-dot = reservedOp ","
-minus = reservedOp "-"
-plus = reservedOp "+"
+comma = rsym Comma ","
+dot = rsym Dot "."
+minus = rsym Minus "-"
+plus = rsym Plus "+"
